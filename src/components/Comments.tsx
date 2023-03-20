f168acc8b373534a5c1dc0a1d628ae530f664991
Timur Ermolaev <timkogas@gmail.com>
2023-03-20 18:26:51 +0300

    Исправление бага с оверлеем, добавил тестовый комментарий

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,11 +1,12 @@
-import { Button, FormItem, Input, PanelHeader, Textarea } from "@vkontakte/vkui"
+import { Avatar, Button, Card, Cell, FormItem, Input, PanelHeader, RichCell, Spacing, Text, Textarea } from "@vkontakte/vkui"
 import { useState } from "react";
+import User from "../store/User";
 
 export const Comments = ({ active, activeCommentsToggle }) => {
   const [comment, setComment] = useState('');
 
   const onChange = (e: React.FormEvent<HTMLTextAreaElement>) => {
-    const {value } = e.currentTarget;
+    const { value } = e.currentTarget;
     setComment(value)
   };
 
@@ -13,10 +14,11 @@ export const Comments = ({ active, activeCommentsToggle }) => {
     e.stopPropagation()
   }
 
+  const [yyyy, mm, dd, hh, mi] = new Date().toISOString().split(/[/:\-T]/)
   return (<>
     <div
       className={"comments-overlay"}
-      style={{ backgroundColor: active ? 'rgb(0,0,0,0.5)' : 'rgb(0,0,0,0)' }}
+      style={{ backgroundColor: active ? 'rgb(0,0,0,0.5)' : 'rgb(0,0,0,0)', visibility: active ? 'visible' : 'hidden' }}
       onClick={activeCommentsToggle}>
       <div
         className="comments-block"
@@ -25,13 +27,25 @@ export const Comments = ({ active, activeCommentsToggle }) => {
       >
         <PanelHeader>Комментарии</PanelHeader>
         <FormItem>
-          <Textarea
-            placeholder="Написать комментарий"
-            onChange={onChange}
-            value={comment}
-          />
-          <Button>Отправить</Button>
+          <div style={{ display: 'flex', gap: '30px' }}>
+            <Textarea
+              style={{ flexGrow: 1 }}
+              placeholder="Написать комментарий"
+              onChange={onChange}
+              value={comment}
+            />
+            <Button size='l'>Отправить</Button>
+          </div>
         </FormItem>
+
+        <RichCell
+          text='asdasd asd asda sda s asda sdas das dasd asd asd asda sd asd asdasd asdas dasd asd asd'
+          before={<Avatar src={User.getUser().photo_100} />}
+        >
+          <Text weight='2'>
+            {`${User.getNickname()} ${dd}/${mm}/${yyyy} ${hh}:${mi}`}
+          </Text>
+        </RichCell>
       </div >
     </div >
   </>
