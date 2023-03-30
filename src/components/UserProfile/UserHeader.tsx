459c476d1f111e5b3eb4303ed649cb1beaae5d3b
Timur Ermolaev <timkogas@gmail.com>
2023-03-30 10:55:47 +0300

    Подправил профиль пользователя для мобильной версии

--- src/components/UserProfile/UserHeader.tsx
+++ src/components/UserProfile/UserHeader.tsx
@@ -6,21 +6,21 @@ import '../../css/profile.css'
 
 export const UserHeader = observer(({ color }: { color: string }) => {
   return (
-    <Div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'row', alignItems: 'center', gap: '70px' }}>
+    <Div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'row', alignItems: 'center', gap: '50px' }}>
       <div className='profile-info-item'>
         <Text>Топ недели</Text>
-        <Icon28CupOutline fill={color} style={{transform: 'scale(2.5)', margin: '20px 0'}}/>
+        <Icon28CupOutline fill={color} style={{transform: 'scale(2)', margin: '15px 0'}}/>
         <Text>{State.getUserProfile().top_week}</Text>
       </div>
       <div className='profile-info-item'>
         <Text weight="1" style={{marginLeft: '12px' }}>
           {State.getUserProfile().name}
         </Text>
-        <Avatar src={State.getUserProfile().avatar} style={{ marginTop: '10px', marginLeft: '12px' }} size={96} />
+        <Avatar src={State.getUserProfile().avatar} style={{ marginTop: '10px', marginLeft: '15px' }} size={96} />
       </div>
       <div className='profile-info-item'>
         <Text>Топ участников</Text>
-        <Icon28CupOutline fill={color} style={{transform: 'scale(2.5)', margin: '20px 0'}}/>
+        <Icon28CupOutline fill={color} style={{transform: 'scale(2)', margin: '15px 0'}}/>
         <Text>{State.getUserProfile().top_all}</Text>
       </div>
     </Div>
